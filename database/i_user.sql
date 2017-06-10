33fe5d2ac1c00fcb3a6e15a63509d7d25ae81f8b
zjkmxy <zjkmxy@163.com>
2017-06-10 23:13:28 +0800

    完善了数据表的设计

--- database/i_user.sql
+++
@@ -1,7 +0,0 @@
-create table USER(
-	id int not null,
-	name varchar(50) not null,
-	password varchar(50) not null,
-	privilege int not null,
-	primary key(id)
-);
--- database/notice.sql
+++
@@ -1,8 +0,0 @@
- CREATE TABLE NOTICE(
-	id int not null,
-	title nvarchar(50) not null,
-	content  nvarchar(2000) not null,
-	time datetime null,
-	unit nvarchar(50) not null,
-	PRIMARY KEY (id)
- );
